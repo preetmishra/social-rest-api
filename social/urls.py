00202693472343bbdf760cb2e5eac3d0dd8079ba
Preet Mishra <ipreetmishra@gmail.com>
2020-10-19 12:21:17 +0530

    yasg: Add ReDoc documentation UI.

--- social/urls.py
+++ social/urls.py
@@ -13,10 +13,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import permissions
+
+
+DESCRIPTION = "An API to add, manage and authenticate users, and run a feed."
+EMAIL = 'work.wicked@gmail.com'
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Social REST API",
+      default_version='v1',
+      description=DESCRIPTION,
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email=EMAIL),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('social_api.urls')),
+    path('', schema_view.with_ui('redoc'), name='docs'),
 ]
